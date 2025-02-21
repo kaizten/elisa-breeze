a207257b4fa9a7a338d7a2b7d6118ca10c56d153
ElisaBreeze <elisamabre@gmail.com>
2025-02-21 12:58:15 +0000

    code working

--- random-solver/src/main/java/com/kaizten/prmp/solver/Main.java
+++ random-solver/src/main/java/com/kaizten/prmp/solver/Main.java
@@ -1,8 +1,16 @@
 package com.kaizten.prmp.solver;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+import org.json.JSONObject;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kaizten.opt.evaluator.Evaluator;
 import com.kaizten.opt.evaluator.builder.EvaluatorBuilder;
+import com.kaizten.opt.solution.validation.KaiztenSolutionValidator;
 import com.kaizten.prmp.domain.problem.PersonsReducedMobilityProblem;
 import com.kaizten.prmp.domain.solution.PersonsReducedMobilitySolution;
 import com.kaizten.prmp.evaluator.PersonsReducedMobilityProblemEvaluator;
@@ -11,13 +19,6 @@ import com.kaizten.prmp.io.PersonsReducedMobilitySolutionToJson;
 import com.kaizten.prmp.solver.solver.RandomSolver;
 import com.kaizten.utils.json.KaiztenJson;
 import com.kaizten.utils.net.KaiztenURI;
-import com.kaizten.opt.solution.validation.KaiztenSolutionValidator;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Optional;
-
-import org.json.JSONObject;
 
 public class Main {
 
@@ -39,10 +40,12 @@ public class Main {
     }
 
     public static void main(String[] args) throws JsonProcessingException, IOException, URISyntaxException {
-        final String instance = "file:/home/christopher/kaizten/internship/elisa-breeze/data/instance-01.json";
+        final String instance = "file:/Users/elisa/Desktop/Uni/Tercero/Practicas/elisa-breeze/data/instance-01.json";
         PersonsReducedMobilityProblem optimizationProblem = Main.getProblemFromURI(instance);
+        System.out.println(optimizationProblem); 
         RandomSolver solver = new RandomSolver(optimizationProblem);
         PersonsReducedMobilitySolution solution = solver.run();
+        System.out.println(solution);
         JSONObject output = null;
         int statusCode = 0;
         if (solution != null) {
--- random-solver/src/main/java/com/kaizten/prmp/solver/solver/RandomSolver.java
+++ random-solver/src/main/java/com/kaizten/prmp/solver/solver/RandomSolver.java
@@ -1,27 +1,8 @@
 package com.kaizten.prmp.solver.solver;
 
-import com.kaizten.opt.datasource.validation.ProblemInstanceDataSourceValidatorManager;
-import com.kaizten.opt.datasource.validation.ProblemInstanceJsonValidator;
-import com.kaizten.opt.evaluator.Evaluator;
-import com.kaizten.opt.evaluator.builder.EvaluatorBuilder;
-import com.kaizten.opt.problem.validation.ProblemInstanceValidatorManager;
 import com.kaizten.opt.solver.AbstractSolver;
-import com.kaizten.opt.validation.ErrorLevel;
-import com.kaizten.opt.validation.ValidationErrors;
-
 import com.kaizten.prmp.domain.problem.PersonsReducedMobilityProblem;
 import com.kaizten.prmp.domain.solution.PersonsReducedMobilitySolution;
-import com.kaizten.prmp.io.PersonsReducedMobilityProblemJsonFileSupplier;
-
-import com.kaizten.utils.net.KaiztenURI;
-import com.kaizten.utils.string.KaiztenString;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.time.LocalDate;
-import java.util.Optional;
-
-import org.json.JSONObject;
 
 public class RandomSolver extends AbstractSolver<PersonsReducedMobilitySolution> {
 
