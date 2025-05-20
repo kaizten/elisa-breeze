2ad3987c5c2ebe3d9eca653595e044d2f4bbcff6
ElisaBreeze <elisamabre@gmail.com>
2025-05-20 09:15:36 +0100

    new Instances

--- xlsx-to-json/src/main/java/com/kaizten/prmp/conversor/Controller.java
+++ xlsx-to-json/src/main/java/com/kaizten/prmp/conversor/Controller.java
@@ -13,22 +13,23 @@ public class Controller {
         //final String[] airports = { "MAD", "SPC", "TFS" };
         final String[] airports = { "MAD"};
         //final double[] percentages = {0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1 };
-        final double[] percentages = {0.35};
+        final double[] percentages = {0.2};
         final int[] hoursPerDay = { 4, 6, 8 };
         final int numberOfInstances = 100;
         for (String airport : airports) {
             for (double percentage : percentages) {
                 for (int hours : hoursPerDay) {
-                    for (int instanceNumber = 0; instanceNumber < numberOfInstances; instanceNumber++) {
+                    //for (int instanceNumber = 0; instanceNumber < numberOfInstances; instanceNumber++) {
                         String[] parameters = {
                                 String.valueOf(percentage),
                                 airport,
-                                String.valueOf(instanceNumber),
+                                //String.valueOf(instanceNumber),
+                                String.valueOf(0),
                                 FILE_PATH, INSTANCE_DIRECTORY, String.valueOf(hours)};
-                        System.out.println(instanceNumber + ": Ejecutando Main con porcentaje: " + percentage
-                                + " y aeropuerto: " + airport);
+                        System.out.println(": Ejecutando Main con porcentaje: " + percentage
+                                + ", aeropuerto: " + airport + ", horas: " + hours);
                         Main.main(parameters);
-                    }
+                    //}
                 }
             }
         }
